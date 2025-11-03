c97afc6842ab454abab0af8bdf53aff5c1591841
SandipShimpi001 <sandip.speroinfosystems@gmail.com>
2025-11-03 17:16:15 +0530

    added code

--- Tata_Screening/urls.py
+++ Tata_Screening/urls.py
@@ -20,5 +20,5 @@ from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Screening/', include('Screening.urls')),
-    # path('', index),
+    path('', index, name='index'),
 ]
--- Tata_Screening/views.py
+++ Tata_Screening/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import FileResponse
+import os
+from django.conf import settings
 
 def index(request):
-    return render(request, 'index.html')
+    # Serve React's build index.html from staticfiles
+    file_path = os.path.join(settings.STATIC_ROOT, 'index.html')
+    if os.path.exists(file_path):
+        return FileResponse(open(file_path, 'rb'))
+    return render(request, '404.html', status=404)
