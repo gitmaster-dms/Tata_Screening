94159bfc2f8818b63664b83d100fd6855b264eff
SandipShimpi001 <sandip.speroinfosystems@gmail.com>
2025-11-03 17:35:38 +0530

    add setting file

--- Tata_Screening/views.py
+++ Tata_Screening/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import FileResponse
-import os
-from django.conf import settings
 
 def index(request):
-    # Serve React's build index.html from staticfiles
-    file_path = os.path.join(settings.STATIC_ROOT, 'index.html')
-    if os.path.exists(file_path):
-        return FileResponse(open(file_path, 'rb'))
-    return render(request, '404.html', status=404)
+    return render(request, 'index.html')
