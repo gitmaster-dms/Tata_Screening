603466a3c92f10628d9093f1689e826f3c2594e3
gitmaster-dms <Gitmaster@sperohealthcare.in>
2025-11-04 16:09:29 +0530

    Fix formatting in settings.py

--- Tata_Screening/settings.py
+++ Tata_Screening/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
